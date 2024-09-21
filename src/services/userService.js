0685e503e1cff85886877bfc9134535e180389dc
huynh <huynhnguyen120202@gmail.com>
2024-09-21 12:35:16 +0700

    API Register & Hash Password

---
+++ src/services/userService.js
@@ -0,0 +1,24 @@
+const User = require("../models/user");
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+
+const createUserService = async (name, email, password) => {
+    try {
+        const hashPassword = await bcrypt.hash(password, saltRounds)
+        let result = await User.create({
+            name: name,
+            email: email,
+            password: hashPassword,
+            role: "Huynh"
+        })
+        return result;
+
+    } catch (error) {
+        console.log(error);
+        return null;
+    }
+}
+
+module.exports = {
+    createUserService
+}
