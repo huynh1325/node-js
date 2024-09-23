53961faa05ba3e033051a629a7aaf659c0ad54bc
huynh <huynhnguyen120202@gmail.com>
2024-09-23 14:56:01 +0700

    Middleware

--- src/routes/api.js
+++ src/routes/api.js
@@ -1,10 +1,12 @@
 const express = require('express');
-const { createUser, handleLogin, getUser } = require('../controllers/userController')
+const { createUser, handleLogin, getUser } = require('../controllers/userController');
+const delay = require('../middleware/delay');
 
 const routerAPI = express.Router();
 
+routerAPI.all('*', delay)
 
-routerAPI.get('/', (req, res) => {
+routerAPI.get('/',(req, res) => {
     return res.status(200).json("Hello api")
 });
 
